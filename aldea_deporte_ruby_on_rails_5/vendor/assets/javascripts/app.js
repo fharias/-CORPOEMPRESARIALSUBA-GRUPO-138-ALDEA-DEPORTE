74149eb380a4fc15ccaef9c4b9966f5ad31ba6ff
widres8.github.io <williandres8@gmail.com>
2018-09-12 09:40:36 -0500

    Add template contact in view index

--- aldea_deporte_ruby_on_rails_5/vendor/assets/javascripts/app.js
+++ aldea_deporte_ruby_on_rails_5/vendor/assets/javascripts/app.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    var myDate = new Date();
-    var myYear = myDate.getFullYear();
     
     $('.div__scroll').on('click', function (e) {
 		var target = $(this.hash);
@@ -12,20 +10,8 @@ $(document).ready(function () {
 			return false;
 		}
 	});
-
-	window.onscroll = function () {
-		scrollFunction();
-	};
     
 });
-
-function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById("scroll").style.display = "block";
-	} else {
-		document.getElementById("scroll").style.display = "none";
-	}
-}
 
 function topFunction() {
 	$('html, body').animate({
